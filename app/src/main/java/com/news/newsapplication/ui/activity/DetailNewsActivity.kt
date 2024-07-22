ad41da89e145b1ad4ddcc15b130628817d482d37
Abhi <ravanaabhi@gmail.com>
2024-07-22 21:04:17 +0530

    changes made in xml and codes

--- app/src/main/java/com/news/newsapplication/ui/activity/DetailNewsActivity.kt
+++ app/src/main/java/com/news/newsapplication/ui/activity/DetailNewsActivity.kt
@@ -2,23 +2,85 @@ package com.news.newsapplication.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.news.newsapplication.R
+import com.news.newsapplication.data.local.ItemDao
+import com.news.newsapplication.data.local.ItemEntity
+import com.news.newsapplication.data.model.ArticlesItem
+import com.news.newsapplication.data.model.News
+import com.news.newsapplication.databinding.ActivityDetailNewsBinding
+import com.news.newsapplication.ui.viewmodel.LatestNewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.URL
 
 @AndroidEntryPoint
 class DetailNewsActivity : AppCompatActivity() {
 
+    private val viewModel:LatestNewsViewModel by viewModels()
+
+    private val binding by lazy {
+        ActivityDetailNewsBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail_news)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(binding.root)
+
+        val intent = intent
+        if(intent.hasExtra("articleInfo"))
+        {
+            var common_article:Any
+            var title:String;
+            var description:String;
+            var author:String;
+            var source:String;
+            var publishedAt:String;
+            var urlToImage:String;
+            var url:String
+            var isFavourite:Boolean
+//            if(intent.getStringExtra("source").equals(UrlConstant.HOME_NEWS_REDIRECTION)) {
+//                val article: ArticlesItem = intent.getParcelableExtra<ArticlesItem>("articleInfo")
+//                common_article = article
+//
+//                title = article.title?:""
+//                description = article.description?:""
+//                author = "by ${article.author?:" Anonymous"}"
+//                source = article.source.name?:""
+//                publishedAt = article.publishedAt?:""
+//                urlToImage = article.urlToImage?:""
+//                url = article.url?:""
+//            }
+//            else {
+//                val article: ItemEntity = intent.getParcelableExtra<ItemEntity>("articleInfo")
+//                common_article = article
+//
+//                title = article.title?:""
+//                description = article.description?:""
+//                author = "by ${article.author}"
+//                source = article.sourceId?:""
+//                publishedAt = article.publishedAt?:""
+//                urlToImage = article.urlToImage?:""
+//                url = article.url?:""
+//                isFavourite = true
+//            }
+
+//            binding.newsDescriptionTitle.text = title
+//            binding.newsDescription.text = description
+//            binding.textViewbyauthor.text = author
+//            binding.textViewbynewspaper.text = source
+//            binding.textViewpublished.text = publishedAt
+
+
+            Glide.with(this@DetailNewsActivity).load("").placeholder(R.drawable.place_holder).into(binding.mainBackdrop)
+
+        }
+        else{
+            finish()
         }
 
     }
